0cfc71df5af4564a0f283a0ee03b25f3d3ed8214
Gabriel Fortin <none>
2016-06-18 15:18:17 +0200

    swipes for hardcoded location and "newest" button

--- app/src/main/java/com/example/habi/meteobis/MainActivity.java
+++ app/src/main/java/com/example/habi/meteobis/MainActivity.java
@@ -10,15 +10,46 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.ReadableDuration;
+
 public class MainActivity extends AppCompatActivity {
+
+    // http://www.meteo.pl/um/php/meteorogram_list.php?ntype=0u&fdate=2016061212&row=466&col=232&lang=pl&cname=Krak%F3w
+    // http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=2016061212&row=466&col=232&lang=pl
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final int TOTAL_PAGES = 1000;
+    private static DateTime newestDate;
+
+    static {
+
+        Log.i(TAG, "======== START =========");
+
+        DateTime now = DateTime.now();
+        int year = now.year().get();
+        int month = now.monthOfYear().get();
+        int day = now.dayOfMonth().get();
+        int hour = (now.hourOfDay().get() / 6 - 1) * 6;
+        String rounded = String.format("%d-%02d-%02dT%02d:00", year, month, day, hour);
+        Log.i(TAG, "rounded time: " + rounded);
+        newestDate = DateTime.parse(rounded);
+    }
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -49,14 +80,16 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(TOTAL_PAGES - 1);
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                mViewPager.setCurrentItem(TOTAL_PAGES - 1);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
@@ -83,6 +116,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static  DateTime getNewestDate() {
+        DateTime result = new DateTime(newestDate);
+        Log.v(TAG, "newest date: " + result);
+        return result;
+    }
+
+    public static String formatTime(DateTime dateTime) {
+        int year = dateTime.year().get();
+        int month = dateTime.monthOfYear().get();
+        int day = dateTime.dayOfMonth().get();
+        int hour = dateTime.hourOfDay().get();
+        String result = String.format("%d%02d%02d%02d", year, month, day, hour);
+        Log.v(TAG, "formatted time: " + result);
+        return result;
     }
 
     /**
@@ -114,8 +163,33 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            int pageNum = getArguments().getInt(ARG_SECTION_NUMBER);
+
+            int hoursToShift = 6 * (TOTAL_PAGES - pageNum);
+//            DateTime dateTime = getNewestDate().minusHours(hoursToShift);
+            DateTime dateTime = getNewestDate().minus(Period.hours(hoursToShift));
+            String formattedTime = formatTime(dateTime);
+            Log.d(TAG, "pageNum = " + pageNum + "    " + hoursToShift + "  =>  " + dateTime);
+
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            textView.setText(getString(R.string.section_format, pageNum) + "   " + formattedTime);
+
+            ImageView img = (ImageView) rootView.findViewById(R.id.meteoImg);
+            final String url = "http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=" + formattedTime + "&row=466&col=232&lang=pl";
+            Log.v(TAG, "url:  " + url);
+            Glide.with(this)
+//            Picasso.with(getActivity())
+                    .load(url)
+                    .fitCenter()
+//                    .centerCrop()
+                    .placeholder(android.R.drawable.ic_menu_help)
+                    .crossFade()
+                    .into(img);
+//                    .into(img, new Callback() {
+//                        @Override public void onSuccess() { Log.i(TAG, "img loading: success   url: " + url); }
+//                        @Override public void onError() { Log.w(TAG, "img loading: error   url: " + url); }
+//                    });
+
             return rootView;
         }
     }
@@ -132,6 +206,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
+            Log.v(TAG, "getItem: " + position);
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
@@ -139,8 +214,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return TOTAL_PAGES;
         }
 
         @Override
